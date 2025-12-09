18ec0cab88397107347ae6fc4399d56b78603a3d
NiteshBabu <n.babu@clerkswell.com>
2025-12-09 19:11:10 +0530

    fix: update client API endpoint to use dynamic base URL based on environment

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,4 +2,8 @@ import { treaty } from '@elysiajs/eden'
 import type { app } from '../app/api/[[...slugs]]/route'
 
 // this require .api to enter /api prefix
-export const client = treaty<app>("/api").api
+export const client = treaty<app>(
+	process.env.NODE_ENV === 'production'
+		? process.env.NEXT_PUBLIC_BASE_URL!
+		: 'localhost:3000'
+).api
